3427458099c6c01a93bb388d72a54a7e49cd161e
Billy Chen <billychen10@hotmail.com>
2020-08-25 17:28:41 +1000

    Cleaned up endpoints and functionality
    pollService.js
    - added getters and setters
    - added submissions array
    
    index.js
    - cleaned up CORS middleware
    - added submitAnswer endpoint

--- src/services/pollService.js
+++ src/services/pollService.js
@@ -1,9 +1,32 @@
-var myPoll = {
+var poll = {
     question: '',
     selections: [],
     answer: null
 };
 
+var submissions = [0, 0, 0, 0];
+
+/* Getters and setters */
+
+const getPoll = () => {
+    return poll;
+}
+
+const setPoll = (pollData) => {
+    poll = pollData;
+}
+
+const submitAnswer = (answer) => {
+    submissions[answer] += 1;
+}
+
+const viewSubmissions = () => {
+    return submissions;
+}
+
 module.exports = {
-    poll: myPoll
+    getPoll: getPoll,
+    setPoll: setPoll,
+    submitAnswer: submitAnswer,
+    viewSubmissions: viewSubmissions
 }
